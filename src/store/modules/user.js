7c71a034f963cdfe8491115ca1eb9d0cd1e4233f
lixiaobia <980243530@qq.com>
2022-07-13 17:21:32 +0800

    写登录页面

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,24 @@
-
-const state = {}
+import { login } from '@/api/user'
+const state = {
+  token: null
+}
 
 const mutations = {
-
+  setToken (state, payload) {
+    state.token = payload
+  }
 }
 
 const actions = {
-
+  async login (context, data) {
+    try {
+      const res = await login(data)
+      console.log(res)
+      context.commit('setToken', res.data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
 
 export default {
